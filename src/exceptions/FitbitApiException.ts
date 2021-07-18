1f546c32898f0cc942191f00a3dd9c6c979ea6fb
Michal Ozogán <michal@ozogan.eu>
2021-07-18 10:39:53 +0000

    Fixed: Lint

--- src/exceptions/FitbitApiException.ts
+++ src/exceptions/FitbitApiException.ts
@@ -24,7 +24,7 @@ export default class FitbitApiException extends FitbitException {
     }
 
     public hasError(error: string): boolean {
-        return !!this.getErrors().find((item) => item.errorType === error);
+        return this.getErrors().some((item) => item.errorType === error);
     }
 
     public getResponse(): ApiResponseType<any> {
